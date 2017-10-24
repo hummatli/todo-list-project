a42b95b7456287dbda6c51f384cf98e7e785b968
Sattar Hummatli <settarxan@gmail.com>
2017-10-24 07:49:02 +0400

    Ended up with siple todo list app

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -40,6 +40,11 @@ $("input[type='text']").keypress(function(event){
 	if(event.which === 13) {
 		var todoText = $(this).val()
 		$(this).val("")
-		$("ul").append("<li><span>X</span> " + todoText + "</li>")
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
 	}
 })
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle()
+})
+
